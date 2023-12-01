5a3efe51368b45c947bcd2452b0e5e28c8e9fb97
CSjianbel <jiankarlo99@gmail.com>
2023-12-01 18:48:40 +0800

    refactor: moved angle_control logic into its own file

---
+++ angle_control.c
@@ -0,0 +1,21 @@
+#include "angle_control.h"
+
+#include <math.h>
+
+void updateInputAngle (AngleControl *angle_control) {
+  if (!angle_control->ball_released) {
+    angle_control->arrow_angle += angle_control->angle_speed;
+    if (angle_control->arrow_angle >= MAX_ANGLE || angle_control->arrow_angle <= MIN_ANGLE) {
+      angle_control->angle_speed *= -1;
+    }
+  }
+}
+
+void drawInputAngle (AngleControl angle_control, Ball ball) {
+  if (angle_control.arrow_moving) {
+    DrawLineEx(
+      (Vector2){ ball.x_pos, ball.y_pos },
+      (Vector2){ ball.x_pos + cosf(angle_control.arrow_angle * DEG2RAD) * 100, ball.y_pos - sinf(angle_control.arrow_angle * DEG2RAD) * 100}, 2, RED
+    );
+  }
+}
---
+++ angle_control.h
@@ -0,0 +1,19 @@
+#ifndef ANGLE_CONTROL_H
+#define ANGLE_CONTROL_H
+
+#include "raylib.h"
+
+#include "constants.h"
+#include "ball.h"
+
+typedef struct {
+  float arrow_angle;
+  int angle_speed;
+  bool ball_released;
+  bool arrow_moving;
+} AngleControl;
+
+void drawInputAngle(AngleControl angle_control, Ball ball);
+void updateInputAngle (AngleControl *angle_control);
+
+#endif
