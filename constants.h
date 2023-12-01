24734d7918cee1b28b43bee5c2fc4d34d9d88bf6
CSjianbel <jiankarlo99@gmail.com>
2023-12-01 18:48:55 +0800

    refactor: moved constants into its own header file

---
+++ constants.h
@@ -0,0 +1,28 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+#define SCREEN_WIDTH 1200
+#define SCREEN_HEIGHT 880
+
+#define BALL_RADIUS 35
+#define BALL_DAMPENING 0.99f
+
+// define colors
+#define BALLCOLOR (Color){ 92, 75, 192, 255 } 
+#define BALLDARKCOLOR (Color){ 65, 50, 150, 255 }
+#define BALLSHADOWINNER (Color){ 81, 81, 81, 255 }
+#define BALLSHADOWOUTER (Color){ 0, 0, 0, 0 }
+
+#define PINS_NUM 10
+#define PIN_RADIUS 20
+#define PIN_DISTANCE 30
+
+#define OBSTACLE_NUM 5
+
+// min and max angles for input
+#define MAX_ANGLE 135
+#define MIN_ANGLE 45
+
+#define ROUND_NUM 10
+
+#endif
